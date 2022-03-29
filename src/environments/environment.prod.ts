7c78f1c670e14a7ecf60a37288753f3cec67f6e0
Ogunwobi Philip Ayodeji <ophilip@pajuno.com>
2022-03-29 07:26:41 +0100

    updates to credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:3000/api',
+  accessToken: '<API KEY - https://arcgis.com/ >',
+  tomtomKey: '<API KEY - https://developer.tomtom.com/ >',
+  domain: 'localhost' // or 'your.domain.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  accessToken: '<API KEY - https://arcgis.com/ >',
+  tomtomKey: '<API KEY - https://developer.tomtom.com/ >',
+  domain: 'localhost' // or 'your.domain.com'
 };
 
 /*
